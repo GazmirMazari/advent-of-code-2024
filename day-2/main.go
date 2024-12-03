92bd69c144a1282cf097683e2298c8e35acdcf94
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2024-12-02 21:49:15 -0500

    add second commit

--- day-2/main.go
+++ day-2/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -18,13 +19,35 @@ func main() {
 	defer file.Close()
 
 	tst, err := io.ReadAll(file)
+	
 	if err != nil {
 		log.Fatalf("Failed to read file: %v", err)
+	}
+
+
+	content := string(tst)
+	//split the content into lines
+	lines :=strings.Split(content, "\n")
+
+	for _, line :=range lines {
+		fields := strings.Fields(line)
+
+        // Convert fields to integers
+        numbers := make([]int, 0, len(fields))
+        for _, field := range fields {
+            num, err := strconv.Atoi(field)
+            if err != nil {
+                log.Printf("Error converting field to integer on line %d: %v", lineNumber, err)
+                continue
+            }
+            numbers = append(numbers, num)
+        }
+
 	}
 
 	elapsedTime := time.Since(startTime)
 	fmt.Printf("File reading took %s\n", elapsedTime)
 
 	// Optionally, use the data variable
-	fmt.Println(string(t))
+	fmt.Println(string(tst))
 }
